c35be2b8bd93e281dac85b3103341f999ce618f7
Aarex Tiaokhiao <aarextiaokhiao@gmail.com>
2018-02-23 19:56:28 -0500

    v0.11

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,4 +1,4 @@
-player={version:1,
+player={version:0.11,
 build:1,
 time:0,
 timePlayed:0,
@@ -17,6 +17,10 @@ function load(savefile) {
 	try {
 		savefile=JSON.parse(atob(savefile))
 		
+		if (savefile.version==1&&savefile.build==1) savefile.version=0.11
+		
+		savefile.version=player.version
+		savefile.build=player.build
 		player=savefile
 		console.log('Game loaded!')
 	} catch (e) {
@@ -41,7 +45,28 @@ function importSave() {
 
 function reset() {
 	if (confirm('Are you sure to reset your save? You can\'t undo your action!')) {
+		player.red={points:0,progress:0}
 		player.timePlayed=0
+	}
+}
+
+function formatTime(s) {
+	if (s < 1) {
+		return Math.floor(s*1000)+' milliseconds'
+	} else if (s < 60) {
+		return Math.floor(s*100)/100+' seconds'
+	} else if (s < 3600) {
+		return Math.floor(s/60)+' minutes and '+Math.floor(s%60)+' seconds'
+	} else if (s < 86400) {
+		return Math.floor(s/3600)+' hours, '+Math.floor(s/60%60)+' minutes, and '+Math.floor(s%60)+' seconds'
+	} else if (s < 2629746) {
+		return Math.floor(s/86400)+' days, '+Math.floor(s/3600%24)+' hours, '+Math.floor(s/60%60)+' minutes, and '+Math.floor(s%60)+' seconds'
+	} else if (s < 31556952) {
+		return Math.floor(s/2629746)+' months, '+Math.floor(s%2629746/86400)+' days, '+Math.floor(s%2629746/3600%24)+' hours, '+Math.floor(s%2629746/60%60)+' minutes, and '+Math.floor(s%2629746%60)+' seconds'
+	} else if (s < Number.POSITIVE_INFINITY) {
+		return Math.floor(s/31556952)+' years, '+Math.floor(s/2629746%12)+' months, '+Math.floor(s%2629746/86400)+' days, '+Math.floor(s%2629746/3600%24)+' hours, '+Math.floor(s%2629746/60%60)+' minutes, and '+Math.floor(s%2629746%60)+' seconds'
+	} else {
+		return 'Infinite'
 	}
 }
 
@@ -58,8 +83,12 @@ function gameTick() {
 	}
 	player.time=newTime
 	
-	document.getElementById('bar1number').innerHTML='Red: '+player.red.points
+	updateElement('bar1number','Red: '+player.red.points)
 	document.getElementById('bar1progress').style.width=(1-player.red.progress)*100+'%'
+	
+	if (currentTab=='stats') {
+		updateElement('timePlayed','You have played this game for '+formatTime(player.timePlayed)+'.')
+	}
 }
 
 function gameInit() {
@@ -80,6 +109,6 @@ function gameInit() {
 				updated=true
 			},tickspeed)
 		}
-	},0)
+	},50)
 	setInterval(save,60000)
 }
